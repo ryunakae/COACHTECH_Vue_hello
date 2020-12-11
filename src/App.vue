c574a908629436020b319073f5c149aa0811428d
ryunakae <ryunakae@gmail.com>
2020-12-11 17:53:07 +0900

    complete v-bind

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,27 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>Hello world</h1>
+    <table>
+      <tr><th>名前</th><th>値段</th><th>個数</th></tr>
+      <tr>
+        <td>{{pencil.name}}</td>
+        <td>{{pencil.price}}</td>
+        <td>{{pencil.quantity}}</td>
+      </tr>
+      <tr>
+        <td>{{eraser.name}}</td>
+        <td>{{eraser.price}}</td>
+        <td>{{eraser.quantity}}</td>
+      </tr>
+      <tr>
+        <td>{{note.name}}</td>
+        <td>{{note.price}}</td>
+        <td>{{note.quantity}}</td>
+      </tr>
+    </table>
+    <input type="text" :value="n">
+    <p>合計金額：{{ woTax }}円（税抜）</p>
+     <p>合計金額：{{ wTax }}円（税抜）</p>
   </div>
 </template>
 
@@ -13,8 +34,38 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      pencil: {
+        name: "鉛筆",
+        price: 100,
+        quantity: 0
+      },
+      eraser: {
+        name: "消しゴム",
+        price: 100,
+        quantity: 0
+      },
+      note: {
+        name: "ノート",
+        price: 100,
+        quantity: 0
+      },
+      n: "hello",
+      apple: 200,
+      orange: 100,
+    };
+  },
+  computed: {
+    woTax() {
+      return this.apple + this.orange
+    },
+    wTax() {
+      return this.woTax *1.1
+    }
   }
-}
+};
 </script>
 
 <style>
